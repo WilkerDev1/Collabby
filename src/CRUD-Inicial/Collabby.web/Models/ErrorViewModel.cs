d969f0041c8a2796629833361dc456f08d34c57d
WilkerDev1 <capellancoronadowilker@gmail.com>
2025-02-08 17:48:30 -0400

    Initial Commit
    agregando proyecto ASP.NET core a la solucion

---
+++ src/CRUD-Inicial/Collabby.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Collabby.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
