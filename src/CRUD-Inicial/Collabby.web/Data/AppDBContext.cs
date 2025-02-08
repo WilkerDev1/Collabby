5381f00c43ee333a7e171ddea5642982b82bf093
WilkerDev1 <capellancoronadowilker@gmail.com>
2025-02-08 19:17:27 -0400

    Parametros BD
    Configuracion de Models y Data para tablas y columnas en la base de datos

---
+++ src/CRUD-Inicial/Collabby.web/Data/AppDBContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Collabby.web.Models; 
+
+namespace Collabby.web.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Empleado> Empleados { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Empleado>(tb => {
+                tb.HasKey(colu => colu.IdEmpleado);
+
+                tb.Property(colu => colu.IdEmpleado)
+                .UseIdentityColumn()
+                .ValueGeneratedOnAdd();
+
+                tb.Property(colu => colu.NombreCompleto).HasMaxLength(50);
+                //tb.Property(colu => colu.Correo).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Empleado>().ToTable("Empleado");
+        }
+    }
+}
