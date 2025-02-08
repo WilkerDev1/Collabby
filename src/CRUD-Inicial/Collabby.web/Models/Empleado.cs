59d84f6e624f139ec47a22be6e0dcc7a0e52d923
WilkerDev1 <capellancoronadowilker@gmail.com>
2025-02-08 18:10:20 -0400

    add public class with microsoft framework for SQL

---
+++ src/CRUD-Inicial/Collabby.web/Models/Empleado.cs
@@ -0,0 +1,13 @@
+﻿namespace Collabby.web.Models
+{
+    public class Empleado
+    {
+        public int IdEmpleado { get; set; }
+        public int NombreCompleto { get; set; }
+        public int Correo { get; set; }
+        public DateOnly FechaContrato { get; set; }
+        public bool Activo { get; set; }
+
+
+    }
+}
