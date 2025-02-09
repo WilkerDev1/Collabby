826143fd10512393de3550a3260a903290e8606f
WilkerDev1 <capellancoronadowilker@gmail.com>
2025-02-08 22:02:40 -0400

    method changes and small changes

--- src/CRUD-Inicial/Collabby.web/Program.cs
+++ src/CRUD-Inicial/Collabby.web/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Empleado}/{action=Lista}/{id?}");
 
 app.Run();
